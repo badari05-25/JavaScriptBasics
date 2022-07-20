7137a2f0d9ebf9a004e40576046f832b9c4c629f
Badari <106369056+badari05-25@users.noreply.github.com>
2022-07-20 19:34:54 +0530

    Update problem6.js

--- problem6.js
+++ problem6.js
@@ -1,3 +1,23 @@
+// **PROBLEM 6**
+
+// Write a code to get an integer N and print the sum of values from 1 to N.
+
+// **Input Description:** ꢀ
+
+// A single line contains an integer N.
+
+// **Output Description:** ꢀ
+
+// Print the sum of values from 1 to N.
+
+// **Sample Input :**
+
+// 10
+
+// **Sample Output :** 55
+
+// 3
+
 let num = parseInt(prompt("enter a number"));
 let total = 0;
 for(let i=1;i<=num; i++){

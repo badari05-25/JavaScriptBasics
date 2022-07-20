fea204aeaad9bf9dcb4be2f8192ff25d2ba68543
Badari <106369056+badari05-25@users.noreply.github.com>
2022-07-20 19:33:56 +0530

    Update problem4.js

--- problem4.js
+++ problem4.js
@@ -1,3 +1,34 @@
+// **PROBLEM 4**
+
+// You will be provided with a number. Print the number of days in the month
+
+// corresponding to that number.
+
+// Note: In case the input is February, print 28 days. If the Input is not in valid range
+
+// print "Error".
+
+// **Input Description:** ꢀ
+
+// Input n -> month number
+
+// **Output Description:** ꢀ
+
+// Find the days in the month corresponding to the input number. Print Error if the input
+
+// is not in a valid range.
+
+// **Sample Input :**
+
+// 8
+
+// **Sample Output :**
+
+// 31
+
+// 2
+
+
 let month = parseInt(prompt("enter a month"));
 
 if(month >1 && month <12){

4fdece9cd0d58f74383330618ec1ab31ab753b42
Badari <106369056+badari05-25@users.noreply.github.com>
2022-07-20 19:37:58 +0530

    Update problem12.js

--- problem12.js
+++ problem12.js
@@ -1,3 +1,29 @@
+
+
+// **PROBLEM 12**
+
+// Simi is learning about palindromic numbers. Her teacher gave him the task to count
+
+// all palindromic numbers present in that range.Simi has told you about this and want
+
+// your help. You design an algorithm in order to help simi.
+
+// **Input Description:** ꢀ
+
+// You will be given a number ‘n’
+
+// **Output Description:** ꢀ
+
+// Print the count of all palindromic numbers till ’n’(inclusive)
+
+// **Sample Input :**
+
+// 5
+
+// **Sample Output :**
+
+// 5
+
 function countpalindromicNum(n){
     count=0;
     for(i=1;i<=n;i++){

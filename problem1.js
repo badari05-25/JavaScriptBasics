52354fdf382e0fdf65c93e234e022aad92322c29
Badari <106369056+badari05-25@users.noreply.github.com>
2022-07-20 19:31:14 +0530

    updating question

--- problem1.js
+++ problem1.js
@@ -1,3 +1,24 @@
+// **PROBLEM 1**
+
+// You are provided with a number, "**N**". Find its factorial.
+
+// **Input Description:** ꢀ
+
+// A positive integer is provided as an input.
+
+// **Output Description:** ꢀ
+
+// Print the factorial of the integer.
+
+// **Sample Input :**
+
+// 5
+
+// **Sample Output :**
+
+// 120
+
+
 let n = parseInt(prompt("enter a number"));
 let fact = 1;
 

9718a62556034c8a4b2f4e435bd187a947dd8299
Badari <106369056+badari05-25@users.noreply.github.com>
2022-07-20 19:39:43 +0530

    Update problem8.js

--- problem8.js
+++ problem8.js
@@ -1,3 +1,27 @@
+// **PROBLEM 8**
+
+// \- Let "A" be a string. Remove all the whitespaces and find it's length.
+
+// (Print "***Error***" if **N** = negative value and 0 if **N** = 0).
+
+// **Input Description:** ꢀ
+
+// A string is provide as an input
+
+// **Output Description:** ꢀ
+
+// Remove all the whitespaces and then print the length of the remaining string.
+
+// **Sample Input :**
+
+// Lorem Ipsum
+
+// **Sample Output :**
+
+// 10
+
+// 4
+
 const str = prompt("enter a name");
 
 let total = str.replace(/\s/g,'');
